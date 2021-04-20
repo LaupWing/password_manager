e06839aee92b164cc4b7b05bfccfd1de1be0ffa5
LaupWing <loc_strict@hotmail.com>
2021-04-20 22:53:03 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import Nav from './Nav/Nav'
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
+   const routes = (
+      <Switch>
+         <Route path="/"/>
+         <Route path="/auth"/>
+         <Route path="/"/>
+      </Switch>
+   )
+
    return (
       <div className="App w-screen h-screen bg-black-default flex">
          <Nav/>
---
+++ src/routes/Main.tsx
@@ -0,0 +1,14 @@
+import LogoImage from '../assets/lock.png'
+
+const Main =  function() {
+   return (
+      <div className="h-full bg-black-light border-r-2 border-black">
+         <img src={LogoImage} alt="logo" className="w-20 m-auto my-4"/>
+         <ul className="text-white text-sm p-2">
+            <li className="w-48 p-2 py-1 bg-blue-600 rounded-md">All Items</li>
+         </ul>
+      </div>
+   );
+}
+
+export default Main
