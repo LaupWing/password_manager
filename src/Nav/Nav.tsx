d73671e38861057f6da18818546c5ffd06da6307
LaupWing <loc_strict@hotmail.com>
2021-04-20 21:20:53 +0200

    added react router dom

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -1,8 +1,11 @@
+import LogoImage from '../assets/lock.png'
+
 const Nav =  function() {
    return (
       <div className="h-full bg-black-light border-r-2 border-black">
+         <img src={LogoImage} alt="logo" className="w-20 m-auto my-4"/>
          <ul className="text-white text-sm p-2">
-            <li className="w-48 p-2 py-1 bg-blue-600 rounded-md">Test</li>
+            <li className="w-48 p-2 py-1 bg-blue-600 rounded-md">All Items</li>
          </ul>
       </div>
    );
