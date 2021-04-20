5b7160836eb3ede58cde4bcbe25d4c387ffe269d
LaupWing <loc_strict@hotmail.com>
2021-04-20 15:04:45 +0200

    set custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
    ppurge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
    darkMode: false, // or 'media' or 'class'
    theme: {
+      colors:{
+         lightBlack: '#282828',
+         defaultBlack: '#1E1E1E',
+         lightestBlack: '#404040'
+      },
       extend: {},
    },
    variants: {
