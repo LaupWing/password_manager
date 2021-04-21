a16c2180e5a91df5bd797a13e54c17c5a2eb57f7
LaupWing <loc_strict@hotmail.com>
2021-04-22 00:30:17 +0200

    containerl inline stylse

--- src/Detail/Detail.tsx
+++ src/Detail/Detail.tsx
@@ -1,6 +1,15 @@
+import CSS from 'csstype'
+
 const Detail  = () =>{
+   const containerStyles: CSS.Properties = {
+      maxWidth: '500px'
+   }
+
    return (
-      <div>
+      <div 
+         className="flex flex-col w-full"
+         style={containerStyles}
+      >
          Detail
       </div>
    )
