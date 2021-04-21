28e8df09a78cfda44687494eb6ffe4576d588616
LaupWing <loc_strict@hotmail.com>
2021-04-21 13:31:05 +0200

    Added vault

---
+++ src/Vault/Vault.tsx
@@ -0,0 +1,11 @@
+const Vault =  function() {
+   return (
+      <div className="h-full bg-black-light border-r-2 border-black">
+         <ul className="text-white text-sm p-2">
+            <li className="w-48 p-2 py-1 bg-blue-600 rounded-md">All Items</li>
+         </ul>
+      </div>
+   );
+}
+
+export default Vault
