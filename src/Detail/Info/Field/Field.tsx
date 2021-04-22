a899e820aa8f28b3e22672b725149dc42dedb194
LaupWing <loc_strict@hotmail.com>
2021-04-22 16:10:17 +0200

    Eye

--- src/Detail/Info/Field/Field.tsx
+++ src/Detail/Info/Field/Field.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Eye from './Icons/Eye'
 
 interface FieldType {
    label: string
@@ -10,16 +11,21 @@ const Field = ({label, value, password}: FieldType) =>{
    const [showPassword, setShowPassword] = useState(false)
 
    return (
-      <div className="py-4 px-2 rounded-md hover:bg-black-light text-sm">
-         <h2 className="text-mainGrey font-bold tracking-wider capitalize mb-1">{label}</h2>
-         {password ?
-            <input 
-               type={showPassword ? "text" : "password"}
-               value={value}
-               className="text-white bg-transparent pointer-events-none"
-            /> :
-            <p className="text-white">{value}</p> 
-         }
+      <div className="py-4 px-2 rounded-md hover:bg-black-light text-sm flex items-center justify-between">
+         <div>
+            <h2 className="text-mainGrey font-bold tracking-wider capitalize mb-1">{label}</h2>
+            {password ?
+               <input 
+                  type={showPassword ? "text" : "password"}
+                  value={value}
+                  className="text-white bg-transparent pointer-events-none"
+               /> :
+               <p className="text-white">{value}</p> 
+            }
+         </div>
+         <div className="text-white">
+            <Eye/>
+         </div>
       </div>
    )
 }
