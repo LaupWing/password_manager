2658b5a480a0fc310e605ec8683767a0f38d4692
LaupWing <loc_strict@hotmail.com>
2021-04-23 19:16:54 +0200

    update

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -1,11 +1,26 @@
 import LogoImage from '../assets/lock.png'
+import {NavLink} from 'react-router-dom'
 
 const Nav =  function() {
    return (
       <div className="h-full bg-black-light border-r-2 border-black">
          <img src={LogoImage} alt="logo" className="w-20 m-auto my-4"/>
-         <ul className="text-white text-sm p-2 px-4">
-            <li className="w-48 p-2 py-1 bg-blue-600 rounded-md">All Items</li>
+         <ul className="text-white text-sm p-2 px-4 flex flex-col">
+            <NavLink 
+               className="w-48 p-2 py-1 rounded-md my-1"
+               activeClassName="bg-blue-600"
+               to="/"
+               exact
+               >
+               All Items
+            </NavLink>
+            <NavLink 
+               className="w-48 p-2 py-1 rounded-md my-1"
+               to="/favorites"
+               activeClassName="bg-blue-600"
+            >
+               Favorites
+            </NavLink>
          </ul>
       </div>
    );
