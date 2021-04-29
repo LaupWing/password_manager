ee1a3fb88e9879d8a02e5e6e04cd990c00761c14
LaupWing <loc_strict@hotmail.com>
2021-04-29 18:24:32 +0200

    update

--- src/routes/Favorites.tsx
+++ src/routes/Favorites.tsx
@@ -1,11 +1,14 @@
 import Vault from '@/components/Vault/Vault'
+import {useSelector} from 'react-redux'
+import {State} from '@/store/reducers/types/State'
+import {FavoriteState} from '@/store/reducers/types/parts/Favorite'
 
 const Favorites= () => {
+   const {favorites} = useSelector<State, FavoriteState>((state)=>state.favorites)
    return (
-      // <Vault
-      //    passwords={favorites}
-      // />
-      <div></div>
+      <Vault
+         passwords={favorites}
+      />
    );
 }
 
